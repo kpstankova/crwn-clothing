d876dd269521d0a134b37ce6a05d37f3615b7717
kpstankova <krasimira579@gmail.com>
2020-10-07 19:59:45 +0300

    sign up added

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import './App.css';
 
 //other
 import { Route, Switch } from 'react-router-dom';
-import {auth} from './firebase/firebase.utils'
+import {auth, createUserProfileDocument} from './firebase/firebase.utils'
 
 class App extends React.Component {
   constructor(){
@@ -25,12 +25,25 @@ class App extends React.Component {
 
   unsubscribeFromAuth=null
 
-  componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user)
-    })
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+
+          console.log(this.state);
+        });
+      }
+
+      this.setState({ currentUser: userAuth });
+    });
   }
 
   componentWillUnmount(){
