3a18005a5f1f18c4865914dcb7b1a559e8c36710
kpstankova <krasimira579@gmail.com>
2020-10-08 16:01:23 +0300

    Added dropdown cart component

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -2,7 +2,8 @@ import userEvent from '@testing-library/user-event'
 import {combineReducers} from 'redux'
 
 import userReducer from './user/user.reduser'
-
+import cartReducer from './cart/cart.reducer'
 export default combineReducers({
-    user: userReducer
+    user: userReducer,
+    cart:cartReducer
 })
